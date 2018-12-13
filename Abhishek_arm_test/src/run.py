5fb40fff241580d81bda7f1c8f3d2253630bfaca
abhishekakkur <abhishek.akkur@gmail.com>
2018-12-13 13:53:34 -0800

    adding correlation

--- Abhishek_arm_test/src/run.py
+++ Abhishek_arm_test/src/run.py
@@ -8,6 +8,6 @@ parser.add_argument('p_num', nargs='?', type=int, default=0,
 args = parser.parse_args()
 
 if args.p_num == 0 or args.p_num == 5:
-    p05b(tau=250,
+    p05b(tau=5,
          train_path='output/flights_pass_1_na_0.csv',
-         eval_path='testinput/flights_new_till_03dec.csv')
+         eval_path='testinput/all_test_with_failures_clean.csv')
